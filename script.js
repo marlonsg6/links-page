dc55c3507082aa8ac156e9594632104b6ef4229d
marlonsg6 <marlonguedes4@gmail.com>
2024-08-05 13:57:18 -0300

    add switch buttton and adjust width container

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  //pegar tag
+  // const img = document.querySelector('#profile img')
+
+  //substituir img
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "")
+  // } else {
+  //   img.setAttribute("src", "./assets/foto-perfil-marlon.png")
+  // }
+}
